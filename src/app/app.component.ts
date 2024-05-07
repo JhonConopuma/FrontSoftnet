64e5de3ab63e96b05e2a044ae765e49a9fbae20c
JhonConopuma <jhon.cibertec@gmail.com>
2024-05-07 08:56:27 -0500

    pruebaSoftnet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UsuarioComponent } from "./components/usuario/usuario.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.sass',
+    imports: [RouterOutlet, UsuarioComponent]
 })
 export class AppComponent {
   title = 'ng-crud-usuario';
